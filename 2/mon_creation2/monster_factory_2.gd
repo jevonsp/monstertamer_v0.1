12a43af419ab685427dea0ef48300429565bcf9f
jevonsp <pineapplesituation@gmail.com>
2025-09-05 15:04:56 -0700

    new classes

--- 2/mon_creation2/monster_factory_2.gd
+++ 2/mon_creation2/monster_factory_2.gd
@@ -1,1 +1,6 @@
 class_name MonsterFactory extends Node
+
+func create_monster(monster_data : MonsterData) -> MonsterInstance:
+	var monster = MonsterInstance.new()
+	monster.set_data(monster_data)
+	return monster
---
+++ 2/mon_creation2/monster_instance.gd
@@ -0,0 +1,7 @@
+class_name MonsterInstance extends Node
+
+@export var monster_data : MonsterData
+
+# This func needs to set every piece of data other components will use. NO dependancies!
+func set_data(data : MonsterData) -> void:
+	pass
