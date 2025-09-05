8cdf183fbc97739bd525450a8d9330b681c4215b
jevonsp <pineapplesituation@gmail.com>
2025-09-05 11:38:56 -0700

    working on hiding scenes

--- main_scenes/battle/full_battle_remake.gd
+++ main_scenes/battle/full_battle_remake.gd
@@ -4,7 +4,18 @@ signal is_in_battle
 @export var in_battle : bool = false
 
 func _ready() -> void:
+	visible = false
 	send_battle_signal()
+
+func _show_subscenes():
+	for child in get_children():
+		child.visible = true
+func _hide_subscenes():
+	for child in get_children():
+		child.visible = false
+
+func check_visibility():
+	await get_tree().process_frame
 
 func send_battle_signal() -> void:
 	if in_battle == true:
