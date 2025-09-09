8b13a5809e18c426c75ef85cbe1f97d6aa324447
jevonsp <pineapplesituation@gmail.com>
2025-09-08 19:40:56 -0700

    added player and map

---
+++ battle2/full_battle_remake.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+signal battle_ready
+
+signal party_requested
+signal battle_ui_requested
+
+@export var in_battle : bool = false
+
+func _ready() -> void:
+	_hide_subscenes()
+	#if in_battle: _show_subscenes()
+
+func _show_subscenes():
+	var nodes = get_tree().get_nodes_in_group("battle_ui")
+	for node in nodes:
+		node.visible = true
+func _hide_subscenes():
+	var nodes = get_tree().get_nodes_in_group("battle_ui")
+	for node in nodes:
+		node.visible = false
+
+func _on_party_requested() -> void:
+	party_requested.emit()
+
+func _on_party_closed() -> void:
+	battle_ui_requested.emit()
+
+func _on_first_party_member_changed(monster: MonsterInstance) -> void:
+	$MonsterUpdater.update_player_monster(monster)
+
+# Make new signal from Encounter Zone
+func _on_battle_monster_recieved(monster: MonsterInstance) -> void:
+	print("updating em1")
+	$MonsterUpdater.update_enemy_monster(monster)
+	in_battle = true
+	battle_ready.emit()
---
+++ test/testing_spawner.gd
@@ -0,0 +1,19 @@
+extends Control
+
+signal monster_made(node)
+signal battle_monster_made(node)
+
+@export var monster_factory : Node
+@export var monster_data : MonsterData
+@export var monster_data2 : MonsterData
+@export var monster_data3 : MonsterData
+
+func _ready() -> void:
+	for i in range(1):
+		var monster = monster_factory.create_monster(monster_data, 1)
+		monster_made.emit(monster)
+		print(monster)
+	for i in range(1):
+		var monster = monster_factory.create_monster(monster_data2, 1)
+		monster_made.emit(monster)
+		print(monster)
